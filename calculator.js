779732cc66b5c1366da5b152460cbf0d84f4ace9
Tamer Nasser <tamernasser@outlook.com>
2019-02-25 23:53:20 +0200

    Fixed typo mistakes

--- calculator.js
+++ calculator.js
@@ -1,17 +1,17 @@
 window.onload = function (){ // Run code once the page is loaded.
 	var buttons = document.getElementsByTagName("input"); // Getting an array of all "input" elements.
-	var displayScreen = buttons[0]; // aAsigning the first input element which is the display screen to a variable.
-	var clear = document.getElementById('clear'); // The clear (c) button
+	var displayScreen = buttons[0]; // assigning the first input element which is the display screen to a variable.
+	var clear = document.getElementById('clear'); // The clear (c) button.
 
-	for(var i=0;i<buttons.length;i++){ // a loop to add onClick listener to the buttons
-		  if(buttons[i].value === '='){ // if the '=' button is clicked then the onclick function will be "calculate()" which I haven't done yet :D
+	for(var i=0;i<buttons.length;i++){ // A loop to add onClick listeners to the buttons.
+		  if(buttons[i].value === '='){ // If the '=' button is clicked then the onclick function will be "calculate()" which I haven't done yet :D
 			    buttons[i].addEventListener("click", calculate());
-      }else{ // else it will be a number or an op so we just add it to the display;
+      }else{ // Else it will be a number or an operation so we just add it to the display screen.
 			   buttons[i].addEventListener("click", addToDisplay(i));
 		  }
 	}
 
-	function addToDisplay (i){ //This function adds the clicked buttin value to the display screen, exepct for clear which clears the screen for sure.
+	function addToDisplay (i){ //This function adds the clicked button value to the display screen, except for 'clear' which clears the display screen for sure.
 		return function(){
 			if (buttons[i].value === "÷") {
                displayScreen.value  +=  "/ " ;
